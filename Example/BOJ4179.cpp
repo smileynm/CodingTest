caa173b175b7473b96638dec5a140d407a598e91
smileynm <smileynm@naver.com>
2025-09-27 01:42:02 +0900

    Add: BOJ4179

--- Example/BOJ4179.cpp
+++ Example/BOJ4179.cpp
@@ -2,14 +2,11 @@
 #include <utility>
 #include <algorithm>
 #include <queue>
-
+#include <cstdlib>
 using namespace std;
 
 #define X   first
 #define Y   second
-
-char board[1000][1000];
-int dist[1000][1000];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -20,9 +17,104 @@ int main() {
 
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
-    
+
+    bool alive = false;
+    int count = 0;
 
     int R, C;
     cin >> R >> C;
+
+    char* board = new char[R * C];
+    int* dist = new int[R * C];
+    int* burned = new int[R * C];
+
+    fill(dist, dist + R * C, 0);
+    fill(burned, burned + R * C, 1000000);
+
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> board[i * C + j];
+            // 불이 입력되면 해당 자리를 1
+            if (board[i * C + j] == 'F') {
+                burned[i * C + j] = 1;
+                fire.push({i, j});
+            }
+            // 지훈이가 입력되면 해당 자리를 1
+            if (board[i * C + j] == 'J') {
+                dist[i * C + j] = 1;
+                jihoon.push({i, j});
+            }
+        }
+    }
+
+    while(!fire.empty()) {
+        pair<int, int> cur = fire.front();
+        fire.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+                continue;
+            }
+            if (board[nx * C + ny] == '#' || burned[nx * C + ny] != 1000000) {
+                continue;
+            }
+            burned[nx * C + ny] = burned[cur.X * C + cur.Y] + 1;
+            fire.push({nx, ny});
+        }
+    }
+    // // 디버깅용 cout
+    // for (int i = 0; i < R; i++) {
+    //     for (int j = 0; j < C; j++) {
+    //         cout << burned[i][j] << " ";
+    //     }
+    //     cout << '\n';
+    // }
+
+    while (!jihoon.empty() && !alive) {
+        pair<int, int> cur = jihoon.front();
+        jihoon.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+                alive = true;
+                count = dist[cur.X * C + cur.Y];
+                break;
+            }
+            if (board[nx * C + ny] == '#' || dist[nx * C + ny] > 0) {
+                continue;
+            }
+            if (burned[nx * C + ny] <= dist[cur.X * C + cur.Y] + 1) {
+                continue;
+            }
+            if (alive) {
+                break;
+            }
+            dist[nx * C + ny] = dist[cur.X * C + cur.Y] + 1;
+            jihoon.push({nx, ny});
+        }
+    }
+    
+    // // 디버깅용 cout
+    // for (int i = 0; i < R; i++) {
+    //     for (int j = 0; j < C; j++) {
+    //         cout << dist[i][j] << " ";
+    //     }
+    //     cout << '\n';
+    // }
+
+    delete[] board;
+    delete[] dist;
+    delete[] burned;
+    
+
+    if (alive) {
+        cout << count;
+    } else {
+        cout << "IMPOSSIBLE";
+    }
+
+
     return 0;
 }
